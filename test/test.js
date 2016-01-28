82ace2eceb5f13b9a558f5e0ced1eac9bd4b5430
Eugene Sharygin <eush77@gmail.com>
2016-01-29 01:04:00 +0300

    test: experiment with mini test runner
    [mini] is a minimalist test runner that attempts to create a minimal API
    for running tests. I thought to replace `mocha` with it, but I found two
    things:
    
      - it's not *the minimal* it could be, it still employs some
        auto-discovery magic and effectively adds a global to the tests;
    
      - it has built-in fancy formatting, with colors... totally unnecessary
        and irrelevant to its goal; I would expect a simpler format or (even
        better) TAP.
    
    [mini]: https://github.com/vesln/mini

--- test/test.js
+++ test/test.js
@@ -5,54 +5,48 @@ var nixt = require('nixt');
 var fs = require('fs');
 
 
-describe('should accept input', function () {
-  it('from file', function (done) {
-    nixt()
-      .run('./cli.js test/data/input.json')
-      .stdout(fs.readFileSync('test/data/output.js', 'utf8').trim())
-      .end(done);
-  });
-
-  it('from stdin', function (done) {
-    nixt()
-      .stdin(fs.readFileSync('test/data/input.json'))
-      .run('./cli.js')
-      .stdout(fs.readFileSync('test/data/output.js', 'utf8').trim())
-      .end(done);
-  });
+test('should accept input from file', function (done) {
+  nixt()
+    .run('./cli.js test/data/input.json')
+    .stdout(fs.readFileSync('test/data/output.js', 'utf8').trim())
+    .end(done);
 });
 
-describe('should raise an error', function () {
-  it('on nonexistent file', function (done) {
-    nixt()
-      .run('./cli.js nonexistent')
-      .code(1)
-      .end(done);
-  });
-
-  it('on invalid JSON input', function (done) {
-    nixt()
-      .stdin('not a JSON')
-      .run('./cli.js')
-      .code(1)
-      .end(done);
-  });
+test('should accept input from stdin', function (done) {
+  nixt()
+    .stdin(fs.readFileSync('test/data/input.json'))
+    .run('./cli.js')
+    .stdout(fs.readFileSync('test/data/output.js', 'utf8').trim())
+    .end(done);
 });
 
-describe('should accept', function () {
-  it('--builder to set custom builder', function (done) {
-    nixt()
-      .stdin(fs.readFileSync('test/data/input.json'))
-      .run('./cli.js --builder=NODE')
-      .stdout(fs.readFileSync('test/data/builder-output.js', 'utf8').trim())
-      .end(done);
-  });
+test('should raise an error on nonexistent file', function (done) {
+  nixt()
+    .run('./cli.js nonexistent')
+    .code(1)
+    .end(done);
+});
 
-  it('format options for escodegen', function (done) {
-    nixt()
-      .stdin(fs.readFileSync('test/data/input.json'))
-      .run('./cli.js --format.indent.style="  " --format.quotes=double')
-      .stdout(fs.readFileSync('test/data/format-output.js', 'utf8').trim())
-      .end(done);
-  });
+test('should raise an error on invalid JSON input', function (done) {
+  nixt()
+    .stdin('not a JSON')
+    .run('./cli.js')
+    .code(1)
+    .end(done);
+});
+
+test('should accept --builder to set custom builder', function (done) {
+  nixt()
+    .stdin(fs.readFileSync('test/data/input.json'))
+    .run('./cli.js --builder=NODE')
+    .stdout(fs.readFileSync('test/data/builder-output.js', 'utf8').trim())
+    .end(done);
+});
+
+test('should accept format options for escodegen', function (done) {
+  nixt()
+    .stdin(fs.readFileSync('test/data/input.json'))
+    .run('./cli.js --format.indent.style="  " --format.quotes=double')
+    .stdout(fs.readFileSync('test/data/format-output.js', 'utf8').trim())
+    .end(done);
 });
